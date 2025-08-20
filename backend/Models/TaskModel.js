f765fc295f2057bcb7a329848ec70df7bbdd35bb
Himanshu Verma <whoishimanshuverma@gmail.com>
2025-08-20 09:17:28 +0530

    Add files via upload

---
+++ backend/Models/TaskModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema({
+    taskName: {
+        type: String,
+        required: true
+    },
+    isDone: {
+        type: Boolean,
+        required: true
+    },
+    priority: {
+        type: String,
+        required: true
+    }
+});
+
+const TaskModel = mongoose.model('todos', TaskSchema);
+module.exports = TaskModel;
